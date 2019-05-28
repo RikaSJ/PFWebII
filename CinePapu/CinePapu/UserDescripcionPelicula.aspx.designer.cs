7ad630ceebbaac41392a01fe10e4e7ee29fa542e
JaredRamirezDiaz <Efrain_jared@hotmail.com>
2019-05-27 21:43:38 -0500

    Complemento de Descripcion de pelicula

--- CinePapu/CinePapu/UserDescripcionPelicula.aspx.designer.cs
+++ CinePapu/CinePapu/UserDescripcionPelicula.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace CinePapu {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label txtTitulo;
+        
+        /// <summary>
+        /// Control Image1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
